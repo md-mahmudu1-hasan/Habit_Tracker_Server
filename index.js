776105eedeb8084d91a8a034e2dff113b0a1b53f
md-mahmudu1-hasan <mdmahmudulhasan0008@gmail.com>
2025-11-12 17:44:53 +0600

    vercel deply setup

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const database = client.db("habitTracker");
     const HabitCollection = database.collection("habits");
@@ -138,7 +138,7 @@ async function run() {
       });
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
